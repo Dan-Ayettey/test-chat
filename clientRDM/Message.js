0064cd9312ffd818eaff226b3725c101950476cd
Dan Ayettey <55326982+Dan-Ayettey@users.noreply.github.com>
2021-02-23 11:05:37 +0100

    update

--- clientRDM/Message.js
+++ clientRDM/Message.js
@@ -1,4 +1,4 @@
-{
+export const message= {
   "patients": [{
     "id": 0,"userId": 0,
     "message": "The numo is good"
--- clientRDM/Patient.js
+++ clientRDM/Patient.js
@@ -1,32 +1,18 @@
-{
-  "patient":
-    {
-      "BCSupport": [
-        { "Id": 0,
-          "UserUID": 0,
-          "Name": "Emmil",
-          "BCSupportTypeId": 0,
-          "Title": "Coach"
-        },
-        { "Id": 1,
-          "UserUID": 1,
-          "Name": "Johan doe",
-          "BCSupportTypeId": 1,
-          "Title": "Trainer"
-        },
-        { "BCSupportType":
-        [
-          {"Id": 0,
-          "Name": "Emmel",
-          "Description": "I am good at coaching, and like sports "
-        },
-          {"Id": 1,
-            "Name": "Johan doe",
-            "Description": "I am good at coaching, and like sports "
-          }
-        ]
-        }
-      ]
-    }
+export const patient=[{
+  "BCSupport":[{
+    "Id": [],
+    "UserUID": [],
+    "Name": [],
+    "BCSupportTypeId": [],
+    "Title": []
+  }
+],
+"BCSupportType":[{
+  "Id": [],
+  "Name": [],
+  "Description": [],
+}]
 
-}
+
+
+}]
